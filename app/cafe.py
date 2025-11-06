d82ca94de648fdd76ea79c22fb5ebe9394c6d5ce
Oleg <oleggavelya@gmail.com>
2025-11-06 10:00:38 +0200

    add return visit_cafe method and add message errors

--- app/cafe.py
+++ app/cafe.py
@@ -7,10 +7,12 @@ class Cafe():
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor: dict) -> None:
+    def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError
-        elif visitor["vaccine"]["expiration_date"] != date.today():
-            raise OutdatedVaccineError
+            raise NotVaccinatedError("All friends should be vaccinated")
+        elif visitor["vaccine"]["expiration_date"] < date.today():
+            raise OutdatedVaccineError("All friends should be vaccinated")
         elif not visitor["wearing_a_mask"]:
             raise NotWearingMaskError
+        else:
+            return f"Friends can go to {self.name}"
